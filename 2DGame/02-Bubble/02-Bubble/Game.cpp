2b2c784f4d3a2321eecfecc840b894feecdb6268
GuillermoLopezEsteve <guillermolopez.2000@gmail.com>
2021-11-03 13:01:26 +0100

    arreglo de bug level

--- 2DGame/02-Bubble/02-Bubble/Game.cpp
+++ 2DGame/02-Bubble/02-Bubble/Game.cpp
@@ -15,7 +15,9 @@ void Game::init()
 bool Game::update(int deltaTime)
 {
 	if (changeLevel) {
+		if (currentLevel > maxLevel) currentLevel = 1;//Loop to first level
 		levelscene.init(currentLevel);
+		changeLevel = false;
 	}
 	if (isLevelscene) {
 		levelscene.update(deltaTime);
